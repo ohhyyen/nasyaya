d44e885ea0ea7398eebedf0e7586512037fe8840
[dyad] <ohhy11d@gmail.com>
2025-07-22 11:31:35 +0800

    [dyad] Mengecilkan saiz fon dan menukar jenis fon tajuk di halaman utama - wrote 3 file(s)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -90,6 +90,9 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        handwriting: ['Caveat', 'cursive'], // Menambah fon tulisan tangan
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
